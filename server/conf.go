1a73de5a4429c0bd8b0bce0db1d16a5c46b6513d
Chylli <chylli.email@gmail.com>
2015-12-08 15:50:35 +0800

    add a default cfg file

--- server/conf.go
+++ server/conf.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/BurntSushi/toml"
+	"os"
 )
 
 type redisConf struct {
@@ -21,6 +22,12 @@ type Configure struct {
 func LoadConfigure() *Configure {
 
 	var file = "../conf/server.cfg"
+	var default_file = "../conf/server_default.cfg"
+
+	if _, err := os.Stat(file); err != nil {
+		file = default_file
+	}
+
 	var config = Configure{}
 
 	if _, err := toml.DecodeFile(file, &config); err != nil {
